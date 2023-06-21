b94e0e783dd3d15ddcc0fe4f8c019a0e8cb9d878
capugliani <capugliani@gmail.com>
2023-06-21 15:26:40 -0300

    exercise added

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+word_list = ["lemon", "strawberry", "bird", "brasil"]
+random_word = random.choice(word_list)
+letters_list = len(list(random_word))
+
+underline_list = []
+for letter in random_word:
+    underline_list.append("_")
+
+lives = 7
+right_guesses = 0
+counting_end = 0
+guessed_letters = []
+chosen_letters_list = []
+
+while lives != counting_end and right_guesses != letters_list:
+    print(f"The word is:\n{underline_list}")
+    guess = input("Guess a letter: ").lower()
+    chosen_letters_list.insert(0, guess)
+    print(f"Letters already choosen:\n{chosen_letters_list}")
+
+    code_running = 0
+    checking_list = 0
+    checking_list2 = 0
+    for item in guessed_letters:
+        if guess == guessed_letters[checking_list]:
+            guessed_letters.remove(guess)
+            right_guesses -= 1
+        checking_list += 1
+        
+    wrong_letter_count = 0
+
+    for letter in random_word:
+        if guess == random_word[code_running]:
+            right_guesses += 1
+            underline_list[code_running] = guess
+            guessed_letters.append(guess)
+            counting_end -= 1
+        code_running += 1
+    counting_end += 1
+
+print(underline_list)
+print("GAME OVER!")
