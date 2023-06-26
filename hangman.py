ca312159b01e02f4382cff451c2a85fe31a03410
capugliani <capugliani@gmail.com>
2023-06-26 12:42:50 -0300

    solved a bug about equal guesses

--- hangman.py
+++ hangman.py
@@ -1,5 +1,63 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''']
+
 word_list = ["lemon", "strawberry", "bird", "brasil"]
 random_word = random.choice(word_list)
 letters_list = len(list(random_word))
@@ -8,20 +66,23 @@ underline_list = []
 for letter in random_word:
     underline_list.append("_")
 
-lives = 7
+lives = 6
 right_guesses = 0
-counting_end = 0
+end_game = 0
 guessed_letters = []
 chosen_letters_list = []
 
-while lives != counting_end and right_guesses != letters_list:
+while lives != end_game and right_guesses != letters_list:
+    print(stages[end_game])
     print(f"The word is:\n{underline_list}")
     guess = input("Guess a letter: ").lower()
-    chosen_letters_list.insert(0, guess)
+    chosen_letters_list.append(guess)
     print(f"Letters already chosen:\n{chosen_letters_list}")
 
     code_running = 0
     checking_list = 0
+    checking_chosen_list = 0
+    repeated_letter = 0
 
     for item in guessed_letters:
         if guess == guessed_letters[checking_list]:
@@ -30,20 +91,27 @@ while lives != counting_end and right_guesses != letters_list:
             print("You already tried this letter, Please, Try another!")
         checking_list += 1
 
+    for item in chosen_letters_list:
+        if repeated_letter >= 1:
+            if guess == chosen_letters_list[checking_chosen_list]:
+                chosen_letters_list.remove(guess)
+                checking_chosen_list -= 1
+                repeated_letter -= 1
+        if guess == chosen_letters_list[checking_chosen_list]:
+            repeated_letter = 1
+        checking_chosen_list += 1
+
     for letter in random_word:
         if guess == random_word[code_running]:
             right_guesses += 1
             underline_list[code_running] = guess
             guessed_letters.append(guess)
-            counting_end -= 1
         code_running += 1
-    counting_end += 1
 
-    if counting_end == 7:
-        print("YOU LOSE!")
+    end_game = len(chosen_letters_list) - len(guessed_letters)
+
+    if end_game == lives:
+        print("YOU LOSE!\nThe word was:", random_word.capitalize(), "\nYour tries were:", underline_list)
     if right_guesses == letters_list:
-        print("YOU WIN!")
+        print(f"YOU WIN!\n{underline_list}\nThe word was: {random_word.capitalize()}")
 
-print(underline_list)
-print("\nThe word was:")
-print(random_word)
