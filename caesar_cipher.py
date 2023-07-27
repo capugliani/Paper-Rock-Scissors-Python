23437edcbe420fca5e1e133a005d2405205235a8
capugliani <capugliani@gmail.com>
2023-07-27 14:26:28 -0300

    fixed overnumbers and spaces

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,6 +4,16 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+reseting_shift = 1
+full_loops = 0
+
+for shift in range(1, shift+1):
+    if reseting_shift == 26:
+        reseting_shift = 1
+        full_loops += 1
+    reseting_shift += 1
+fixing_number = shift - (full_loops * 26)
+
 def encrypt(text_function, shift_function):
     encrypt_counting = 0
     crypto_list = []
@@ -11,11 +21,13 @@ def encrypt(text_function, shift_function):
 
     for item in encrypting_text:
         letter_counting = 0
+        if item == ' ':
+            crypto_list.append(' ')
         for letter in alphabet:
             if encrypting_text[encrypt_counting] == alphabet[letter_counting]:
-                if letter_counting+shift >= 25:
+                if letter_counting+shift_function >= 25:
                     actual_letter = letter_counting+shift_function - 25
-                    crypto_list.append(alphabet[actual_letter-1])
+                    crypto_list.append(alphabet[actual_letter])
                 else:
                     crypto_list.append(alphabet[letter_counting + shift_function])
             letter_counting += 1
@@ -31,9 +43,9 @@ def decrypt(text_function, shift_function):
         letter_counting = 0
         for letter in alphabet:
             if decrypting_text[decrypt_counting] == alphabet[letter_counting]:
-                if letter_counting+shift >= 25:
+                if letter_counting+shift_function >= 25:
                     actual_letter = letter_counting-shift_function - 25
-                    decrypto_list.append(alphabet[actual_letter-1])
+                    decrypto_list.append(alphabet[actual_letter])
                 else:
                     decrypto_list.append(alphabet[letter_counting-shift_function])
             letter_counting += 1
@@ -41,9 +53,9 @@ def decrypt(text_function, shift_function):
     print(''.join(decrypto_list))
 
 if direction == 'encode':
-    encrypt(text_function=text, shift_function=shift)
+    encrypt(text_function=text, shift_function=fixing_number)
 elif direction == 'decode':
-    decrypt(text_function=text, shift_function=shift)
+    decrypt(text_function=text, shift_function=fixing_number)
 else:
     print("Sorry, but i didn't understand if you want to encode or decode!")
 
